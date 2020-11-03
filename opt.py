b87137904f6d1042cf84583eeb31c527d4a02c3b
Mathias Åsberg <mathias.asberg@gmail.com>
2020-11-03 23:45:10 +0100

    sync

---
+++ opt.py
@@ -0,0 +1,13 @@
+import optuna
+
+
+def objective(trial):
+    x = trial.suggest_uniform('x', -10, 10)
+    return (x - 2) ** 2
+
+
+study = optuna.create_study()
+study.optimize(objective, n_trials=100)
+
+
+print(study.best_params)
